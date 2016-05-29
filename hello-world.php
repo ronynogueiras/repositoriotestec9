3b2a2ad020453955b71f72605de3d92f3d105f66
TESTE <contasanonimas123@gmail.com>
2016-05-29 19:10:10 +0000

    t

--- hello-world.php
+++ hello-world.php
@@ -3,7 +3,7 @@
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-echo 'Hello World s';
+echo 'Hello World ass';
 ?>
 </body>
 </html>
